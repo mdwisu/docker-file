6307dfa9926f30233a99c7ae6f7b063daa626a25
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-27 20:14:22 +0700

    chore: run instruction

--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
+# FROM Instruction
 docker build -t dwismu/from from
 
 docker image ls
+
+# RUN Instruction
+docker build -t dwismu/run run
+
+docker build -t dwismu/run run --progress=plain --no-cache
