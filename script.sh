ee8bc216ab365d1afa63f00b3d196c03c219a7a5
Muhammad Dwi Susanto <dwisusanto784@gmail.com>
2024-12-27 20:52:23 +0700

    chore: label instruction

--- script.sh
+++ script.sh
@@ -1,11 +1,14 @@
 # FROM Instruction
-docker build -t dwismu/from from
+docker build -t dwismu/from 1.from
 docker image ls
 # RUN Instruction
-docker build -t dwismu/run run
-docker build -t dwismu/run run --progress=plain --no-cache
+docker build -t dwismu/run 2.run
+docker build -t dwismu/run 2.run --progress=plain --no-cache
 # CMD Instruction
-docker build -t dwismu/command command
+docker build -t dwismu/command 3.command
 docker container create --name command dwismu/command
 docker container start command
 docker container logs command
+# LABEL Instruction
+docker build -t dwismu/label 4.label
+docker image inspect dwismu/label
